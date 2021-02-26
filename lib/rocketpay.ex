5bfd712f123905926aeded3c1b61738c4694b499
Lysandro Carioca <lysandroc@gmail.com>
2021-02-26 10:06:08 -0300

    Withdraw endpoint implementation

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,9 +1,10 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
 
-  alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.{Deposit, WithDraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: WithDraw, as: :call
 end
