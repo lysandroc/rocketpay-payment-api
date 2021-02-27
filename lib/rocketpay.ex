530b3ccfa207dbad4561671603391662aee0ba00
Lysandro Carioca <lysandroc@gmail.com>
2021-02-26 21:10:39 -0300

    Transactions endpoint

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,10 +1,11 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
 
-  alias Rocketpay.Accounts.{Deposit, WithDraw}
+  alias Rocketpay.Accounts.{Deposit, Transaction, WithDraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withdraw(params), to: WithDraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
