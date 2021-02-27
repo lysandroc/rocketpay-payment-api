2ef06ee702b5c70129240b87f9cfccbe19b49477
Lysandro Carioca <lysandroc@gmail.com>
2021-02-27 14:01:29 -0300

    Enabling user creation test

--- test/rocketpay/users/create_test.exs
+++ test/rocketpay/users/create_test.exs
@@ -5,20 +5,20 @@ defmodule Rocketpay.Users.CreateTest do
   alias Rocketpay.Users.Create
 
   describe "call/1" do
-    # test "when all params are valid, returns an user" do
-    #   params = %{
-    #     name: "Name one",
-    #     password: "123456",
-    #     nickname: "nameOne",
-    #     email: "test@banana.com",
-    #     age: 27
-    #   }
+    test "when all params are valid, returns an user" do
+      params = %{
+        name: "Name one",
+        password: "123456",
+        nickname: "nameOne",
+        email: "test@banana.com",
+        age: 27
+      }
 
-    #   {:ok, %User{id: user_id}} = Create.call(params)
-    #   user = Repo.get(User, user_id)
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
 
-    #   assert %User{name: "Name one", age: 27, id: ˆuser_id} = user
-    # end
+      assert %User{name: "Name one", age: 27, id: ˆuser_id} = user
+    end
 
     test "when there are invalid params, returns an error" do
       params = %{
